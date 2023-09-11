b7cbe12ba6ac7381439cd90abe91d6b2a11afc6a
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-11 10:39:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_ponavljanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 7;
+            double c = 12.5, d = 16.7;
+            Console.WriteLine((a + b + c + d) / 4);
+            Console.ReadKey();
+        }
+    }
+}
